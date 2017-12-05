90c4aa0ee6921c0056f76b8cbc482e1d356be2ed
oliver.velich <bielovico@gmail.com>
2017-12-05 16:47:37 +0100

    Chenged script for preparing sessions data and created a new file with modified sessions. Each session contains information about time spent in different modes.

--- prepare_sessions.py
+++ prepare_sessions.py
@@ -8,7 +8,8 @@ days = pd.date_range(first_day, last_day)
 number_of_days = len(days)
 
 sessions_list = []
-columns = ['date', 'session_start', 'session_end', 'session_length']
+columns = ['date', 'session_start', 'session_end', 'session_length',
+           'mode0', 'mode1', 'mode2', 'mode3', 'mode4', 'mode5', 'mode6', 'mode7', 'mode8']
 
 for day in days:
     date = day.date()
@@ -17,7 +18,7 @@ for day in days:
     start = day + td(9, 'h')
     end = day + td(18, 'h')
 
-    points_filename = '%s.%s\max_values_%s-%s.txt' % (day.day, day.month, day.day, day.month)
+    points_filename = 'original data\%s.%s\max_values_%s-%s.txt' % (day.day, day.month, day.day, day.month)
     points = pd.read_csv(points_filename, header=None, names=['overpoints', 'timestamp'], parse_dates=[1])
     if points.empty:
         continue
@@ -27,15 +28,29 @@ for day in days:
     points['difference'] = (points.timestamp - points.previous_timestamp).fillna(0)
     points['difference_seconds'] = points['difference'].apply(lambda x: x.seconds)
 
+    full_filename = 'online visualization/full_%s-%s.csv' % (day.day, day.month)
+    full = pd.read_csv(full_filename, parse_dates=[1])
+    full['time'] = full['time'].apply(pd.Timestamp.time)
+
     s_start = points.head(1).time.item()
     p_time = s_start
 
+    modes = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
     for i, row in points.iterrows():
         if row.difference > td(3, 's'):
+            vc = full['mode_id'][(full['time'] >= s_start) & (full['time'] <= p_time)].value_counts()
+            for j in range(len(modes)):
+                if vc.get(j) is not None:
+                    modes[j] = vc.get(j)
 
-            sessions_list.append([date, s_start, p_time,
-                                  (pd.Timestamp.combine(date, p_time) - pd.Timestamp.combine(date, s_start)).seconds+1])
+            session = [date, s_start, p_time,
+                       (pd.Timestamp.combine(date, p_time) - pd.Timestamp.combine(date, s_start)).seconds + 1]
+            session.extend(modes)
+            sessions_list.append(session)
+
             s_start = row.time
+            modes = [0, 0, 0, 0, 0, 0, 0, 0, 0]
         p_time = row.time
 
 sessions = pd.DataFrame(data=sessions_list, columns=columns)
