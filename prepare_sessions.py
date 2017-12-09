f5f15fe58c4c384a36abd4702ae942cdd6dfa62f
oliver.velich <bielovico@gmail.com>
2017-12-09 21:22:35 +0100

    Modified script for preparing Sessions and added a new file. Sessions now contain number of mode changes per second and length of longest interaction.

--- prepare_sessions.py
+++ prepare_sessions.py
@@ -3,12 +3,12 @@ from numpy import timedelta64 as td
 import numpy as np
 
 first_day = pd.Timestamp('26-04-2017')
-last_day = pd.Timestamp('2017-06-05')
+last_day = pd.Timestamp('05-06-2017')
 days = pd.date_range(first_day, last_day)
 number_of_days = len(days)
 
 sessions_list = []
-columns = ['date', 'session_start', 'session_end', 'session_length',
+columns = ['date', 'session_start', 'session_end', 'session_length', 'longest_length', 'mode_changes_per_second',
            'mode0', 'mode1', 'mode2', 'mode3', 'mode4', 'mode5', 'mode6', 'mode7', 'mode8']
 
 for day in days:
@@ -36,23 +36,41 @@ for day in days:
     p_time = s_start
 
     modes = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    longest_in_mode = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    mode_changes = 0
+    current_mode = 0
+    current_length = 0
 
-    for i, row in points.iterrows():
+    for _, row in points.iterrows():
         if row.difference > td(3, 's'):
-            vc = full['mode_id'][(full['time'] >= s_start) & (full['time'] <= p_time)].value_counts()
-            for j in range(len(modes)):
-                if vc.get(j) is not None:
-                    modes[j] = vc.get(j)
+            full_session = full['mode_id'][(full['time'] >= s_start) & (full['time'] <= p_time)]
+            current_mode = full_session.iloc[0]
+            for i in range(len(full_session)):
+                if full_session.iloc[i] != current_mode:
+                    mode_changes += 1
+                    if current_length > longest_in_mode[full_session.iloc[i]]:
+                        longest_in_mode[current_mode] = current_length
+                    current_length = 0
+                    current_mode = full_session.iloc[i]
+                modes[full_session.iloc[i]] += 1
+                current_length += 1
+            if current_length > longest_in_mode[full_session.iloc[i]]:
+                longest_in_mode[current_mode] = current_length
 
-            session = [date, s_start, p_time,
-                       (pd.Timestamp.combine(date, p_time) - pd.Timestamp.combine(date, s_start)).seconds + 1]
+            session_length = (pd.Timestamp.combine(date, p_time) - pd.Timestamp.combine(date, s_start)).seconds + 1
+            session = [date, s_start, p_time, session_length, max(longest_in_mode), mode_changes/session_length]
             session.extend(modes)
             sessions_list.append(session)
 
             s_start = row.time
             modes = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+            longest_in_mode = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+            mode_changes = 0
+            current_mode = 0
+            current_length = 0
+
         p_time = row.time
 
 sessions = pd.DataFrame(data=sessions_list, columns=columns)
 
-sessions.to_csv('sessions_continue.csv', index_label='id')
+sessions.to_csv('sessions_continue.csv', index_label='id', float_format='%.4f')
